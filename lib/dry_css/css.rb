f254bb1eb726067bce40bad74d889994bb5f6f65
gpxl <gerlando@gmail.com>
2013-08-12 22:22:10 -0700

    use cssparser regex for colors

--- lib/dry_css/css.rb
+++ lib/dry_css/css.rb
@@ -7,9 +7,16 @@ module DryCss
     end
 
     def colors
+      scan_for(CssParser::RE_COLOUR)
+    end
+
+    private
+
+    def scan_for(property)
+      @property = property
       @values_hash = {}
       @parser.each_selector do |selector, declarations, specificity|
-        if d_val = declarations.scan(/([\w-]+):[ ]?([\w\d]+);/)
+        if d_val = declarations.scan(/([\w-]+):[ ]?(#{@property})/)
           d_val.each do |val|
             if @values_hash[val[1]].nil?
               @values_hash[val[1]] = 1
@@ -19,8 +26,8 @@ module DryCss
           end
         end
       end
-
       return @values_hash.sort_by{|key,val| val}.reverse
     end
+
   end
 end
