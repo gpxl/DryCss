2d28ad283c1ebdf9c56fbec6d59de5f8da858738
gpxl <gerlando@gmail.com>
2013-08-20 05:26:49 -0700

    added threading to uri loading

--- lib/dry_css/css.rb
+++ lib/dry_css/css.rb
@@ -1,11 +1,8 @@
 module DryCss
   class CSS
     attr_reader :parser
-    def initialize(*uri)
-      @parser = DryCss::Parser.new
-      uri.each do |u|
-        @parser.load_uri!(u)
-      end
+    def initialize(*uris)
+      @parser = load_uris(uris)
     end
 
     def colors
@@ -18,16 +15,27 @@ module DryCss
 
     private
 
-    def scan_for(property)
-      @property = property
-      @values_hash = {:counts => Hash.new(0), :total => 0}
-      @parser.each_selector do |selector, declarations, specificity|
-        declarations.scan(/([\w-]+):[ ]?(#{@property})/) do |prop,val|
-          @values_hash[:counts][val.to_sym] += 1
-          @values_hash[:total] += 1
+    def load_uris(uris)
+      parser = DryCss::Parser.new
+      threads = []
+      uris.each do |u|
+        threads << Thread.new(u) do |uri|
+          parser.load_uri!(uri)
         end
       end
-      return @values_hash
+      threads.each{|thr| thr.join }
+      return parser
+    end
+
+    def scan_for(property)
+      values_hash = {:counts => Hash.new(0), :total => 0}
+      @parser.each_selector do |selector, declarations, specificity|
+        declarations.scan(/([\w-]+):[ ]?(#{property})/) do |prop,val|
+          values_hash[:counts][val.to_sym] += 1
+          values_hash[:total] += 1
+        end
+      end
+      return values_hash
     end
 
   end
