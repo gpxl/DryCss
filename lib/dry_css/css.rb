afc54f663667e6240ae42123431b0506a9ad3373
gpxl <gerlando@gmail.com>
2013-08-20 04:41:30 -0700

    refactored scan_for method

--- lib/dry_css/css.rb
+++ lib/dry_css/css.rb
@@ -20,17 +20,11 @@ module DryCss
 
     def scan_for(property)
       @property = property
-      @values_hash = {:counts => {}, :total => 0}
+      @values_hash = {:counts => Hash.new(0), :total => 0}
       @parser.each_selector do |selector, declarations, specificity|
-        if d_val = declarations.scan(/([\w-]+):[ ]?(#{@property})/)
-          d_val.each do |val|
-            if @values_hash[:counts][val[1].to_sym].nil?
-              @values_hash[:counts].merge!(val[1].to_sym => 1)
-            else
-              @values_hash[:counts][val[1].to_sym] += 1
-            end
-            @values_hash[:total] += 1
-          end
+        declarations.scan(/([\w-]+):[ ]?(#{@property})/) do |prop,val|
+          @values_hash[:counts][val.to_sym] += 1
+          @values_hash[:total] += 1
         end
       end
       return @values_hash
